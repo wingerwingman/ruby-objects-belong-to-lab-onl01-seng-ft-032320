5e4af331fdd15d6cba2d9505415ef4585fed8772
Justin Winger <justinwinger4@gmail.com>
2020-04-04 15:34:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,9 @@
 require 'pry'
 class Artist 
   attr_accessor :name
-  def name(name)
-    @name = name
-  end
   
-  # def initialize(name)
-  #   binding.pry
-  #   # @name = name
-  # end 
+  def initialize(name)
+    binding.pry
+    # @name = name
+  end 
 end 
